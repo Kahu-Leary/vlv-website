01b419beac376f0b3fe4e48f0334087c2d21fe10
Kahu Leary <kahu.leary@outlook.com>
2022-02-22 16:35:37 +1300

    Update file structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const VLVApp = ({ Component, pageProps }) => {
+  return (
+      <>
+      <Head>
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1" 
+        />
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" 
+          crossOrigin="anonymous" 
+          referrerPolicy="no-referrer" 
+        />
+        <link
+          rel="shortcut icon"
+          type="image/svg"
+          href="/img/hotel-solid.svg"
+        />  
+        <title>Vision Luxury Villas | True Relaxation</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default VLVApp 
